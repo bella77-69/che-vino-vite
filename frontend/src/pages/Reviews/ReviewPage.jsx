4a2e01959c66c8509776ee5350a0a225c8526702
bella77-69 <mrspasceri@hotmail.com>
2024-01-02 20:34:05 -0800

    working on styling the reviews page

--- frontend/src/pages/Reviews/ReviewPage.jsx
+++ frontend/src/pages/Reviews/ReviewPage.jsx
@@ -49,41 +49,154 @@ const ReviewPage = () => {
   };
 
   return (
-    <section className="wine dark:bg-[#343434]">
-      <div className="d-flex justify-content-center container">
-        <div className="card p-3 text-white">
-          <div className="about-product">
-            {items.map((item) => (
-              <div className="mt-0 mt-4" key={item.id}>
-                {/* {currentItem.id === item.id && (
-                  <ReviewsList items={items} listId={item.id} />
-                )} */}
-                <img
-                  className="pl-3"
-                  src={item.image}
-                  width="100"
-                  alt="wine-img"
-                />
+    // <section className="wine dark:bg-[#343434]">
+    //   <div className="d-flex justify-content-center container">
+    //     <div className="card p-3 text-white">
+    //       <div className="about-product">
+    //         {items.map((item) => (
+    //           <div className="mt-0 mt-4" key={item.id}>
+    //             {/* {currentItem.id === item.id && (
+    //               <ReviewsList items={items} listId={item.id} />
+    //             )} */}
+    //             <img
+    //               className="pl-3"
+    //               src={item.image}
+    //               width="100"
+    //               alt="wine-img"
+    //             />
 
-                <h4 className="card-title mt-4 mx-3">{item.wine}</h4>
-                <h5 className="card-subtitle mx-3 mt-2">{item.style}</h5>
-                <h5 className="card-subtitle mx-3 mt-2">{item.price}</h5>
-                <h5 className="card-subtitle mx-3 mt-2 text-muted font-italic">
-                  {item.review}
-                </h5>
-                <p className="card-text p-y-1 mx-3 mt-2">
-                  Rating: {item.rating}
-                </p>
-                <button
-                  className="btn mx-3 mt-2 border-dark"
-                  onClick={(e) => handleSubmit(item.id, e)}
-                >
-                  <Link to={`/comments/${item.id}`} className="card-link">
-                    More Info
-                  </Link>
-                </button>
+    //             <h4 className="card-title mt-4 mx-3">{item.wine}</h4>
+    //             <h5 className="card-subtitle mx-3 mt-2">{item.style}</h5>
+    //             <h5 className="card-subtitle mx-3 mt-2">{item.price}</h5>
+    //             <h5 className="card-subtitle mx-3 mt-2 text-muted font-italic">
+    //               {item.review}
+    //             </h5>
+    //             <p className="card-text p-y-1 mx-3 mt-2">
+    //               Rating: {item.rating}
+    //             </p>
+    //             <button
+    //               className="btn mx-3 mt-2 border-dark"
+    //               onClick={(e) => handleSubmit(item.id, e)}
+    //             >
+    //               <Link to={`/comments/${item.id}`} className="card-link">
+    //                 More Info
+    //               </Link>
+    //             </button>
+    //           </div>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+    <section class="flex items-center bg-gray-200 min-h-screen font-poppins dark:bg-gray-900 w-full">
+      <div class="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
+        <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 md:grid-cols-2">
+          {items.map((item) => (
+            <div
+              class="relative bg-white rounded-2xl dark:bg-gray-700"
+              key={item.id}
+            >
+              <div class="w-full h-56 px-4 pt-4">
+                <img
+                  src={item.image}
+                  alt=""
+                  class="object-cover w-full h-full rounded-lg "
+                />
               </div>
-            ))}
+              <div>
+                <div class="p-4">
+                  <div class="mb-3">
+                    <a href="">
+                      <h2 class="text-2xl font-semibold dark:text-gray-300">
+                        {item.wine}
+                      </h2>
+                    </a>
+                  </div>
+                  <p class="pb-16 text-base font-base text-gray-700 dark:text-gray-400">
+                    {item.review}
+                  </p>
+                  <h6 className="card-subtitle mx-3 mt-2">{item.style}</h6>
+                  <h6 className="card-subtitle mx-3 mt-2">{item.price}</h6>
+                  <p className="card-text p-y-1 mx-3 mt-2">
+                    Rating: {item.rating}
+                  </p>
+                </div>
+                <div class="flex items-center justify-end ">
+                  <button
+                    onClick={(e) => handleSubmit(item.id, e)}
+                    class="absolute bottom-0 right-0 px-3 py-4 text-sm text-gray-100 bg-indigo-600 rounded-tl-2xl rounded-br-2xl hover:bg-indigo-700 hover:text-gray-100"
+                  >
+                    <Link to={`/comments/${item.id}`} className="card-link">
+                      More Info
+                    </Link>
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div class="relative bg-white rounded-2xl dark:bg-gray-700">
+            <div class="w-full h-56 px-4 pt-4 ">
+              <img
+                src="https://i.postimg.cc/s2tvtrPF/first.jpg"
+                alt=""
+                class="object-cover w-full h-full rounded-lg "
+              />
+            </div>
+            <div>
+              <div class="p-4">
+                <div class="mb-3">
+                  <a href="">
+                    <h2 class="text-2xl font-semibold dark:text-gray-300">
+                      Lorem ipsum dor amet ispiciousas
+                    </h2>
+                  </a>
+                </div>
+                <p class="pb-16 text-base font-base text-gray-700 dark:text-gray-400">
+                  We will take you different places of nepal and will translate
+                  japanese to english entertain ...
+                </p>
+              </div>
+              <div class="flex items-center justify-end ">
+                <a
+                  href="#"
+                  class="absolute bottom-0 right-0 px-3 py-4 text-sm text-gray-100 bg-indigo-600 rounded-tl-2xl rounded-br-2xl hover:bg-indigo-700 hover:text-gray-100"
+                >
+                  Find out more
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="relative bg-white rounded-2xl dark:bg-gray-700">
+            <div class="w-full h-56 px-4 pt-4 ">
+              <img
+                src="https://i.postimg.cc/Qdhgyp8g/second.jpg"
+                alt=""
+                class="object-cover w-full h-full rounded-lg "
+              />
+            </div>
+            <div>
+              <div class="p-4">
+                <div class="mb-3">
+                  <a href="">
+                    <h2 class="text-2xl font-semibold dark:text-gray-300">
+                      Lorem ipsum dor amet ispiciousas
+                    </h2>
+                  </a>
+                </div>
+                <p class="pb-16 text-base font-base text-gray-700 dark:text-gray-400">
+                  We will take you different places of nepal and will translate
+                  japanese to english entertain ...
+                </p>
+              </div>
+              <div class="flex items-center justify-end ">
+                <a
+                  href="#"
+                  class="absolute bottom-0 right-0 px-3 py-4 text-sm text-gray-100 bg-indigo-600 rounded-tl-2xl rounded-br-2xl hover:bg-indigo-700 hover:text-gray-100"
+                >
+                  Find out more
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
